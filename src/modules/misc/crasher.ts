faa1812df46decd1b04ed4e52aa0bdf71cd755ce
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-12-27 23:10:29 +0900

    add custom crasher trigger value

--- src/modules/misc/crasher.ts
+++ src/modules/misc/crasher.ts
@@ -4,6 +4,7 @@ import { events } from "bdsx/event";
 import { ModuleBase, ModuleConfig } from "../base";
 
 const UINT32_MAX = 0xFFFFFFFF;
+const CRASHER_TRIGGER = 0x7FFFFFFF;
 
 export default class Crasher extends ModuleBase {
     configModel = class Config extends ModuleConfig {
@@ -18,10 +19,9 @@ export default class Crasher extends ModuleBase {
 
         punish.generic=Crasher is detected.
     */};
-    
     load(): void {
         this.listen(events.packetBefore(MinecraftPacketIds.PlayerAuthInput), (pk, ni) => {
-            if ((pk.moveX > UINT32_MAX && pk.moveZ > UINT32_MAX) || (pk.pos.x > UINT32_MAX && pk.pos.y > UINT32_MAX&& pk.pos.z > UINT32_MAX)) {
+            if ((pk.moveX > CRASHER_TRIGGER && pk.moveZ > CRASHER_TRIGGER) || (pk.pos.x > CRASHER_TRIGGER && pk.pos.y > CRASHER_TRIGGER&& pk.pos.z > CRASHER_TRIGGER)) {
                 this.suspect(ni, this.translate("suspect.invalidCoordinates"));
                 if (this.getConfig().punish) {
                     this.punish(ni, this.translate("punish.generic"));
